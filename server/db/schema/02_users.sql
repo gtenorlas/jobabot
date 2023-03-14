b7764afcb32bc62f879e6e6ad6c9de1f38b7c4af
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-03-13 20:37:19 -0400

    feature: implement db/schema/02_users.sql

--- server/db/schema/02_users.sql
+++ server/db/schema/02_users.sql
@@ -5,5 +5,8 @@ CREATE TABLE
     id SERIAL PRIMARY KEY NOT NULL,
     user_level_id INT NOT NULL REFERENCES user_levels(id) ON DELETE CASCADE,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
   );
