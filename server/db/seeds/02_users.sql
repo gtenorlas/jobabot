bfec8e5d60ffa173d03829085fc5deeee18ada98
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-03-13 21:17:10 -0400

    feature: implement db/seeds/02_users.sql

---
+++ server/db/seeds/02_users.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+  users(email, password)
+VALUES
+(
+    1,
+    'abc@example.com',
+    '$2b$12$64.etdeMf7RrrxKTu8UoQu1YkHEaHtcubCOBRLfwqTpXMh7ACB0uS'
+  ),
+(
+    2,
+    'admin@example.com',
+    '$2b$12$64.etdeMf7RrrxKTu8UoQu1YkHEaHtcubCOBRLfwqTpXMh7ACB0uS'
+  );
