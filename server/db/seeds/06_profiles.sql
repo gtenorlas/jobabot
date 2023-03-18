8db9b0686287e2590ffe346b1542f7271ba01b02
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-03-17 22:43:38 -0400

    feature: implement seeds/06_profiles.sql

---
+++ server/db/seeds/06_profiles.sql
@@ -0,0 +1,39 @@
+INSERT INTO
+  profiles(
+    user_id,
+    first_name,
+    last_name,
+    phone_number,
+    linkedin,
+    github,
+    website,
+    city,
+    province,
+    country
+  )
+VALUES
+  (
+    1,
+    'George',
+    'Lucas',
+    '111-123-4767',
+    null,
+    null,
+    null,
+    'Toronto',
+    'ON',
+    'Canada'
+  ),
+VALUES
+  (
+    2,
+    'Jane',
+    'Doe',
+    '133-123-4767',
+    null,
+    null,
+    null,
+    'New York City',
+    'NY',
+    'United States'
+  );
