cee91fb93696b532a8da1fb7a1f441d4676c5ac6
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-03-22 20:58:18 -0400

    feature: implement db/seeds/01_user_levels.sql

--- server/db/seeds/01_user_levels.sql
+++ server/db/seeds/01_user_levels.sql
@@ -1,7 +1,7 @@
 INSERT INTO
   user_levels (
-    title VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL
+    title,
+    description
   )
 VALUES
   ('applicant', 'Regular user seeking for a job'),
