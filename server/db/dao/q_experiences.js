2962f8c8eb669be9e967b64f1073bbd9d7aa881a
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-06-20 22:15:26 -0400

    implement getExperiencesByUserId

---
+++ server/db/dao/q_experiences.js
@@ -0,0 +1,81 @@
+const db = require('../connection')
+
+/**
+ * Get all experiences by id that are still active in db
+ * @param (number) id, the user id
+ * @return (Promise<{}>) A promise of all the experiences pertaining to the user
+ */
+export const getExperiencesByUserId = id => {
+  let query = `
+  SELECT
+  *
+FROM
+  experiences
+WHERE
+  user_id = ?
+  AND deleted_at IS NULL;
+  `
+
+  const params = [id]
+  return db.query(query, params).then(data => data.rows)
+}
+
+/**
+ * Update existing skill
+ * @param(json) skill data
+ * @return(Promise<{}>) A promise of updated skill
+ */
+export const updateSkill = data => {
+  let query = `
+  update
+    skills
+  SET
+    title = $1,
+    category = $2,
+    updated_at = NOW()
+  WHERE
+    id = $3
+  RETURNING *;
+  `
+
+  const params = [data.title, data.category, data.id]
+  return db.query(query, params).then(data => data.rows[0])
+}
+
+/**
+ * Delete existing skill
+ * @param(json) skill data
+ * @return(Promise<{}>) A promise of updated skill
+ */
+export const deleteSkill = data => {
+  let query = `
+  update
+    skills
+  SET
+    deleted_at = NOW()
+  WHERE
+    id = $1
+  RETURNING *;
+  `
+  const params = [data.id]
+  return db.query(query, params).then(data => data.rows[0])
+}
+
+/**
+ * Insert new skill
+ * @param(json) skill data
+ * @return(Promise<{}>) A promise of created skill
+ */
+export const createSkill = data => {
+  const query = `
+  INSERT INTO
+    skills (user_id, title, category)
+  VALUES
+    ($1, $2, $3) RETURNING *;
+  `
+
+  const params = [data.user_id, data.title, data.category]
+  return db.query(query, params).then(data => data.rows[0])
+}
+
+
